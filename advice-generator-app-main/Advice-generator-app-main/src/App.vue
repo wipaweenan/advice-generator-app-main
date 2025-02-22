c6781b7ece1f412f5c832ee481b62cff27f3825e
wipaweenan <nanwipawee2545@gmail.com>
2025-02-22 18:26:36 +0700

    first commit

---
+++ advice-generator-app-main/Advice-generator-app-main/src/App.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+import SvgImage from '/src/images/pattern-divider-desktop.svg';
+import Dice from '/src/images/icon-dice.svg';
+
+const items = ref({});
+
+const fetchAdvice = async () => {
+  try {
+    const response = await axios.get('https://api.adviceslip.com/advice');
+    items.value = response.data.slip;
+    console.log("New Advice:", response.data.slip);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+// เรียก API ครั้งแรกเมื่อ Component โหลด
+onMounted(fetchAdvice);
+</script>
+
+<template>
+  <div class="container">
+    <span class="title">ADVICE# {{ items.id }}</span>
+    <span class="quote">"{{ items.advice }}"</span>
+    <img :src="SvgImage" alt="divider" />
+    <div class="button" @click="fetchAdvice">
+      <img class="dice" :src="Dice" alt="dice button" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: auto;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  background-color: #323a49;
+  border-radius: 10px;
+  padding: 20px;
+  padding-bottom: 64px;
+  gap: 24px;
+  max-width: 500px;
+}
+.title {
+  text-align: center;
+  font-size: 15px;
+  letter-spacing: 5px;
+  font-weight: 600;
+  color: #52ffa8;
+}
+.quote {
+  color: #cde3e9;
+  font-size: 28px;
+  font-weight: 700;
+  text-align: center;
+}
+.dice {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.button {
+  border-radius: 100%;
+  background: #52ffa8;
+  width: 4rem;
+  height: 4rem;
+  position: absolute;
+  left: 50%;
+  bottom: -30px;
+  transform: translateX(-50%);
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+.button:hover {
+  box-shadow: 0px 0px 30px rgba(82, 255, 168, 0.8);
+}
+</style>
